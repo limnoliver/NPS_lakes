180a3321416a045b56776be1b6559716fad6d171
Oliver <soliver@usgs.gov>
2017-06-01 15:39:08 -0500

    reverted code back to annual

--- code/parks_airtemp_annual.R
+++ code/parks_airtemp_annual.R
@@ -5,9 +5,8 @@ library(RColorBrewer)
 library(dplyr)
 set_config(config(ssl_verifypeer = 0L))
 
-# fetch prism mean air temp monthly data
-get_prism_monthlys(type = 'tmean', years = 1980:2014, mon = c(7:9), keepZip = FALSE)
-
+# fetch prism mean air temp annual data
+get_prism_annual(type = 'tmean', years = 1980:2014, keepZip = FALSE)
 
 # create list of long/lats for parks
 parks <- list(ACDA = c(-68.25, 44.36), VOYA = c(-93.38, 48.60), 
@@ -15,9 +14,10 @@ parks <- list(ACDA = c(-68.25, 44.36), VOYA = c(-93.38, 48.60),
               OLYM = c(-123.6, 47.8))
 
 
-# for monthly data, only use files with month attached to file name
-# which distinguishes between monthly and annual data files
-files.keep <- grep('_[[:digit:]]{6}_', ls_prism_data()$files)
+# for yearly data, only use files with YYYY attached to file name
+# which distinguishes between monthly (YYYYMM) and annual data files
+# also distinguishes mean from tmin
+files.keep <- grep('PRISM_tmean_stable_4kmM2_[[:digit:]]{4}_', ls_prism_data()$files)
 
 # extract climate data for each park location
 parks.climate.mean <- list()
@@ -26,39 +26,29 @@ for (i in 1:length(parks)){
   parks.climate.mean[[i]] <- prism_slice(parks[[i]], ls_prism_data()[files.keep,1])$data
 }
 
-
-# find annual summer averages
-summer_annual <- list()
-for (i in 1:length(parks)){
-  summer_annual[[i]] <- parks.climate.mean[[i]] %>%
-    mutate(., year = format(date, "%Y")) %>%
-    group_by(year) %>%
-    summarise(summer_avg = mean(data))
-}
-
 # create data frame from list
-summer.df <- do.call(rbind, lapply(summer_annual, data.frame))
-summer.df$park <- rep(names(parks), each = length(1980:2014)-1) #1982 is missing
+annual.df <- do.call(rbind, lapply(parks.climate.mean, data.frame))
+annual.df$park <- rep(names(parks), each = length(1980:2014)-1) #1982 is missing
 
 # write df
-write.csv(summer.df, "data/parks_summer_meanairtemp.csv")
+write.csv(annual.df, "data/parks_annual_meanairtemp.csv")
 
 # plot annual tmean data for each park on same plot
 # keep temperature scale of each plot
-park.cols <- brewer.pal(8, name = 'Dark2')
+park.cols <- brewer.pal(6, name = 'Dark2')
 
-png('figures/parks_summer_meantemp_realtemp.png', height = 6, width = 4, units = 'in', res = 300)
+png('figures/parks_annual_meantemp.png', height = 6, width = 4, units = 'in', res = 300)
 par(xpd = TRUE, mar = c(3,3,1,1), mgp = c(2,0.5,0), tcl = -0.3)
 
-plot(summer_avg~year, type = 'l', col = park.cols[1], 
-     lwd = 3, ylim = c(7,32), xlab = "Year", ylab = 'Summer Mean Temperature', 
-     bty = "L", data = summer.df[summer.df$park == names(parks)[1],]) 
+plot(data~date, type = 'l', col = park.cols[1], 
+     lwd = 3, ylim = c(-2,23), xlab = "Year", ylab = 'Annual Mean Temperature', 
+     bty = "L", data = annual.df[annual.df$park == names(parks)[1],]) 
 
 for (i in 2:length(parks)){
-  points(summer_avg~year, type = 'l', col = park.cols[i], lwd = 3, data = summer.df[summer.df$park == names(parks)[i],])
+  points(data~date, type = 'l', col = park.cols[i], lwd = 3, data = annual.df[annual.df$park == names(parks)[i],])
 }
-legend(x = 1980, y = 33, legend = names(parks), lwd = 3, 
-       col = park.cols, ncol = 4, cex = 0.5)
+legend(x = as.Date("1980-01-01"), y = 24.7, legend = names(parks), lwd = 3, 
+       col = park.cols, ncol = 4, cex = 0.7)
 
 dev.off()
 
